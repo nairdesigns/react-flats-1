9c4dcd2d807f7ee49ebe3ab07d9eceef39232f69
Brian Gonzales <37412568+nairdesigns@users.noreply.github.com>
2022-04-14 10:23:18 +0000

    added basic boiler play features

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import './components/Flat'
 
 function App() {
   return (
     <main>
       React⚛️ + Vite⚡ + Replit🌀
+      <Flat/>
     </main>
   );
 }
